528305351d1d5a9cfeeaef212c988ff9e5951d1c
gerritt <gmocore@gmail.com>
2020-05-22 10:48:23 -0600

    post actions

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { createPost } from '../actions/postActions'
 
 class PostForm extends Component {
     constructor(props) {
@@ -25,15 +28,7 @@ class PostForm extends Component {
             body: this.state.body
         }
 
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(post)
-        })
-        .then(res => res.json())
-        .then(data => console.log(data))
+        this.props.createPost(post)
 
     }
 
@@ -63,4 +58,8 @@ class PostForm extends Component {
     }
 }
 
-export default PostForm
+PostForm.propTypes = {
+    createPost: PropTypes.func.isRequired
+}
+
+export default connect(null, {createPost})(PostForm)
